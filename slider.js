2515e577f77ead35e8e2f5b551882e0a6c5a4344
andrivash <andr.ivas12@gmail.com>
2017-03-01 13:15:23 +0200

    add data

--- slider.js
+++ slider.js
@@ -1,1 +1,6 @@
-
+var images = [
+    'https://images.pexels.com/photos/6675/flight-mountains-sky-flying.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://www.pexels.com/photo/sky-clouds-wind-windmill-9135/',
+    'https://images.pexels.com/photos/66997/pexels-photo-66997.jpeg?h=350&auto=compress&cs=tinysrgb',
+    'https://www.pexels.com/photo/field-sky-nature-agriculture-7976/'
+]
