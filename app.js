8fe34185992869819ada0518c8a947851b5400f8
andrivash <andr.ivas12@gmail.com>
2017-03-01 22:03:38 +0200

    add js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log('app is ready');
 
-console.log(Form.f());
+var form = document.querySelector("#forom");
+if (form !== null){
+    form.addEventListener('submit', function(evt){
+       evt.preventDefault();
+       console.log('submit');
+        var result = validation.init(form);
+        if(result) {
+            Server.send(form);
+        }
+    });
+
+}
+
--- form.js
+++ form.js
@@ -1,19 +1,35 @@
 var Validation = (function(){
+    function _init(form){
+        console.log('validation init');
+        var field = document.querySelectorAll('input[type="text"]');
+        return _validate(field).length ? true: false
+    }
+    function _validate(field) {
+        var result = Array.prototype.slice.call(field);
+        return result.filter(_filter);
+    }
+    function _filter(elem){
+        return elem.value !== ''
+    }
 
     return {
-
+        init: _init,
+        add: _add,
+        remove: _remove
     }
 })();
 window.validation = Validation;
 /*
  1.Наличие данный в форме  (событие от пользователя)
-    - есть ли форма на странице, какая конкретная форма
-    - проверка какие есть поля в форме
-    - отбираем валидируемые
-    - запускаем проверку <===  результат (2.)
-    - проверка результата
-    - если ок то отправляем на сревер
-    -если все плохо, то выводим ошибку
+ - есть ли форма на странице, какая конкретная форма
+ var form =
+
+ - проверка какие есть поля в форме
+ - отбираем валидируемые
+ - запускаем проверку <===  результат (2.)
+ - проверка результата
+ - если ок то отправляем на сревер
+ -если все плохо, то выводим ошибку
 
  2. Корректность ввода данных согласно требованиям
      - взять данные
