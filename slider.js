d05e2ec4396f9ae651bcac2211a1fdb1d64deee0
andrivash <andr.ivas12@gmail.com>
2017-03-01 21:22:38 +0200

    add algo

--- slider.js
+++ slider.js
@@ -3,5 +3,5 @@ var images = [
     'https://static.pexels.com/photos/9135/sky-clouds-blue-horizon.jpg',
     'https://images.pexels.com/photos/66997/pexels-photo-66997.jpeg?h=350&auto=compress&cs=tinysrgb',
     'https://images.pexels.com/photos/7976/pexels-photo.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
-]
+];
 
